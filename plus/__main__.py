9f641a119efae2c81e885f5ec5a364e5f0b19d51
Daril Rodriguez <darilrodriguez.2@gmail.com>
2023-12-18 10:57:42 -0300

    Improve subcommand descriptions

--- plus/__main__.py
+++ plus/__main__.py
@@ -15,19 +15,19 @@ def main():
     subparsers = parser.add_subparsers()
 
     # init subparser
-    init_parser = subparsers.add_parser('init', help='init help')
+    init_parser = subparsers.add_parser('init', help='create a new c++ project')
     init_parser.add_argument('init_name', help='project name')
     init_parser.add_argument('-l', '--lib', action='store_true', help='create a library project')
     init_parser.add_argument('-s', '--shared-lib', action='store_true', help='create a shared library project')
     init_parser.add_argument('-a', '--app', action='store_true', help='create an application project with no console')
 
     # build subparser
-    build_parser = subparsers.add_parser('build', help='build help')
+    build_parser = subparsers.add_parser('build', help='build the current project or the project specified')
     build_parser.add_argument('build_name', help='project name', default='.', nargs='?')
     build_parser.add_argument('-r', '--release', action='store_true', help='build in release mode')
 
     # run subparser
-    run_parser = subparsers.add_parser('run', help='run help')
+    run_parser = subparsers.add_parser('run', help='run the current project or the project specified')
     run_parser.add_argument('run_name', help='project name', default='.', nargs='?')
     run_parser.add_argument('-r', '--release', action='store_true', help='run in release mode')
     
